47bc58552f010a0e5a8664cb3f60114a30135af0
Mahmud Ali <mahmud.ali@par-tec.it>
2026-04-10 23:33:28 +0200

    Fix watchlist: use full asset data when navigating to TradeScreen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3683883594" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "923297591" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
